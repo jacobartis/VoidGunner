a77481170c8d65a69ac1edc808fb4919c8e540b6
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-11 13:15:05 +0100

    Projectiles now take a list of groups as targets.

--- projectiles/projectile.gd
+++ projectiles/projectile.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-@export var damage:float = 1
 
-var speed: float
-var direction: Vector2
+var target_groups:Array = ["player"]
+var damage:float = 1
+var speed: float = 100
+var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	global_position += speed*direction.normalized()
+	global_position += speed*direction.normalized()*delta
 
 
 func _on_area_2d_body_entered(body):
-	if !body.is_in_group("player") and !body.is_in_group("enemy"): return
+	if target_groups.filter(func(group):return body.is_in_group(group)).is_empty():
+		return
 	body.hit(damage)
 	queue_free()
