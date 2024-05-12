4cbbd06a95ca81ad2dd6e5a6b58a64f646a44856
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-12 17:33:38 +0100

    Added enums for item rarities and types, added func for getting item data.

--- library/knowlage_shop.gd
+++ library/knowlage_shop.gd
@@ -1,15 +1,32 @@
 extends Node
 
+enum Types {
+	Elixer,
+	Spellbook,
+	Weapon,
+}
+
+enum Rarities{
+	Common,
+	Rare,
+	Epic,
+	Legendary
+}
+
 var knowlage: int = 0
-var player
+var player_stats: PlayerStats
 
 func add_knowlage(val):
 	knowlage += val
 	print(knowlage)
 
-func set_player(val):
-	player = val
-
-func get_spells():
-	if !player: return
-	return player.get_spell_manager().get_spells()
+func get_item_data(type:Types):
+	var item_data:ItemData
+	match type:
+		Types.Elixer:
+			item_data = preload("res://library/items/test_elixer.tres")
+		Types.Spellbook:
+			item_data = preload("res://library/items/test_spellbook.tres")
+		Types.Weapon:
+			item_data = preload("res://library/items/test_weapon.tres")
+	return item_data
