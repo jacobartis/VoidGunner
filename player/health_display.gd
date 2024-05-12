6dd134ae32230243c43ba16e3c7efa6ad2556685
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-12 13:00:01 +0100

    Added health and madness bar, madness reduces max health by percentage.

---
+++ player/health_display.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func health_change(value):
+	$Health.value = value
+
+func max_health_change(value):
+	$Health.max_value = value
+
+func madness_change(value):
+	$Madness.value = value
