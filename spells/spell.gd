6ffa41fa1872b936e7a944bd0d1a694b39b81ec8
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-12 13:11:56 +0100

    Base spell class.

---
+++ spells/spell.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Spell
+
+@export_category("Spell Basics")
+@export var cost: float = 10
+@export var cooldown: float = 5
+
+var remaining: float = 0
+
+func can_cast():
+	return remaining<=0
+
+func process(delta):
+	if remaining>0:
+		remaining -= delta
+	else:
+		remaining = 0
+
+func cast(body):
+	remaining = cooldown
