9e0162efb0f23b215b641ff9c99b02e89618b20f
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-13 18:21:05 +0100

    Moved item scripts.

--- library/items/weapon_data.gd
+++
@@ -1,8 +0,0 @@
-extends ItemData
-class_name WeaponItemData
-
-@export_category("Weapon Info")
-@export var weapon_scene: PackedScene
-
-func apply_to(stats:PlayerStats):
-	stats.weapon = weapon_scene
