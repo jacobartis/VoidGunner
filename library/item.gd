260063c854e0c36eddbccb1c7bfd3b035d9e3bc9
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-13 18:14:06 +0100

    Added image and price display for the items.

--- library/item.gd
+++ library/item.gd
@@ -7,10 +7,11 @@ var item_data: ItemData:set=set_item_data
 func set_item_data(new_data):
 	item_data = new_data
 	$ItemName.set_text(item_data.item_name)
+	$Sprite2D.set_texture(item_data.item_texture)
+	$Price.set_text(str(item_data.price))
 
 func _on_area_2d_mouse_entered():
 	$AnimationPlayer.play("mouse_action")
-
 
 func _on_area_2d_mouse_exited():
 	$AnimationPlayer.play_backwards("mouse_action")
