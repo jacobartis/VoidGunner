dcdfe21d9b35460dedd1a4f9880d80a583d91464
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-05-13 18:26:04 +0100

    Added knowlage display, added inventory display.

--- library/shop_controller.gd
+++ library/shop_controller.gd
@@ -2,20 +2,34 @@ extends Node2D
 
 var selected_item: set=set_selected_item
 
+func _ready():
+	KnowlageShop.knowlage_update.connect(update_knowlage_display)
+	update_knowlage_display(KnowlageShop.knowlage)
+	var spawn_locals = get_tree().get_nodes_in_group("ItemSpawn")
+	for x in (randi()%(spawn_locals.size()-1))+1:
+		spawn_locals.pick_random().spawn_item()
+
+func update_knowlage_display(val):
+	%KnowlageDisplay.set_text(str("Knowlage : ",val))
+
 func set_selected_item(new_item):
 	if new_item == selected_item:
-		%BuyButton.hide()
 		return
 	
 	selected_item = new_item
 	var cam_anim = get_tree().create_tween().set_parallel(true)
 	if selected_item:
+		%InventDisplay.hide()
+		toggle_invent_display(false)
+		%BuyButton.disabled = KnowlageShop.knowlage<selected_item.item_data.price
+		%BuyButton.show()
 		cam_anim.tween_property($Camera2D, "global_position",selected_item.global_position,.5)
 		cam_anim.tween_property($Camera2D, "zoom",Vector2(1.5,1.5),.5)
 	else:
+		%InventDisplay.show()
+		%BuyButton.hide()
 		cam_anim.tween_property($Camera2D, "global_position",global_position,.5)
 		cam_anim.tween_property($Camera2D, "zoom",Vector2(1,1),.5)
-	%BuyButton.show()
 
 func _input(event):
 	if event.is_action_pressed("player_cancel") and selected_item:
@@ -25,10 +39,22 @@ func _input(event):
 
 func on_item_selected(item):
 	selected_item = item
-	print(selected_item.item_data)
 
 func on_buy_button_pressed():
 	if !selected_item:
 		push_error("Buy button pressed with no item selected")
 		return
+	KnowlageShop.knowlage -= selected_item.item_data.price
 	selected_item.item_data.apply_to(KnowlageShop.player_stats)
+	selected_item.queue_free()
+	selected_item = null
+
+func toggle_invent_display(toggle:bool):
+	print(toggle)
+	var invent_tween = get_tree().create_tween()
+	if !toggle:
+		invent_tween.tween_property(%InventDisplay, "global_position",Vector2(0,616),1)
+		%ToggleInvent.button_pressed = false
+	else:
+		invent_tween.tween_property(%InventDisplay, "global_position",Vector2(0,83),1)
+
